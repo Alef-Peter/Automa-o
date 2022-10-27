0e11e090e28c0742d5c0277dc80527dec6c1983e
Alef-Peter <alef.araujorj@gmail.com>
2022-10-27 11:41:13 -0200

    Raspagem de dados Amazon

--- Amazon.py
+++ Amazon.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
@@ -8,7 +10,8 @@ tabela = {'Produto': [],
           'Preço': []}
 
 ####INICIANDO O NAVEGADOR####
-navegador = webdriver.Chrome()
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
 navegador.get('https://www.amazon.com.br/s?bbn=16243890011&rh=n%3A16209062011%2Cn%3A%2116209063011%2Cn%3A16243803011%2Cn%3A16243890011%2Cp_89%3AApple&tag=hydrbrgk-20&ref=pd_sl_2f6lzeiv63_e')
 time.sleep(2)
 
@@ -32,8 +35,9 @@ try:
 except:
     print ('Primeiro elemento não encontrado')
 
-####TRANSFORMANDO A TABELA NUM DATAFRAME
+####TRANSFORMANDO A TABELA EM UM DATAFRAME E SALVANDO EM EXCEL####
 tabela_vendas = pd.DataFrame(tabela)
+tabela_vendas.to_excel('Teste.xlsx')
 print(tabela_vendas)
 
 
